2e48418f7b712db25cbbaedddd181e7dee3c8975
Keaton <punmasterkeat@gmail.com>
2025-10-23 16:44:00 -0400

    basic page to page
    implemented page to page connexion

--- app/src/main/java/com/example/munchai/frontend/StartActivity.java
+++ app/src/main/java/com/example/munchai/frontend/StartActivity.java
@@ -1,11 +1,12 @@
 package com.example.munchai.frontend;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.munchai.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity
 {
@@ -13,7 +14,13 @@ public class StartActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.startpage);
+        Button startButton = findViewById(R.id.start_button);
+
+        startButton.setOnClickListener(v ->
+        {
+            Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
