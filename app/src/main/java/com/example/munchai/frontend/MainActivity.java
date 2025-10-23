bafcbe95e3e6f2d8a1fb57edc3b24bebe29cac9b
Keaton <punmasterkeat@gmail.com>
2025-10-23 17:14:43 -0400

    fixes
    fixes

--- app/src/main/java/com/example/munchai/frontend/MainActivity.java
+++ app/src/main/java/com/example/munchai/frontend/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainpage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
         {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
