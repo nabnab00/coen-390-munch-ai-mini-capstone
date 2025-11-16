74c5e46de0949bd218428821eb833f6b01f6cd54
Nabil Khan <149202867+nabnab00@users.noreply.github.com>
2025-11-16 00:27:15 -0500

    Revert "updates to match main code and update the progress circle"

--- app/src/main/java/com/example/munchai/frontend/MainActivity.java
+++ app/src/main/java/com/example/munchai/frontend/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
